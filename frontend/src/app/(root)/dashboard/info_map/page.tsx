afa962ca6b6f81602336014174d722394a7e9ec9
Marin <mikulec.marin52@gmail.com>
2024-11-09 14:58:43 +0200

    buuuus

--- frontend/src/app/(root)/dashboard/info_map/page.tsx
+++ frontend/src/app/(root)/dashboard/info_map/page.tsx
@@ -1,11 +1,14 @@
 'use client'
 import React from 'react'
-import { Map, Marker } from 'pigeon-maps'
+import { Map, Marker, Overlay } from 'pigeon-maps'
+import { BusFront } from '@vectopus/atlas-icons-react'
 
 export default function MyMap() {
     return (
         <Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
-            <Marker width={50} anchor={[50.879, 4.6997]} />
+            <Marker width={25} anchor={[50.879, 4.6997]}>
+                <BusFront color="#2c56a1" weight="bold" />
+            </Marker>
         </Map>
     )
 }
